9059a124735006495dec2a38d1276c521515a1b9
aidant11 <aidant10@nycstudents.net>
2025-03-18 15:49:02 +0000

    123

---
+++ ReviewAnalysis.java
@@ -0,0 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ReviewAnalysis {
+
+    private Review[] allReviews;
+
+    public ReviewAnalysis(Review[] reviews) {
+        allReviews = reviews;
+    }
+
+    ArrayList<String> comments = new ArrayList<String>();
+
+    public double getAverageRating() {
+        double sum = 0;
+        for (Review r : allReviews)
+            sum += r.getRating();
+        return sum / allReviews.length;
+    }
+
+    public ArrayList<String> collectComments() 
+    {
+        for (int i=0; i<allReviews.length; i++)
+        {
+            String s=i + "-" + allReviews[i].getComment():
+        }
+    }
+}
