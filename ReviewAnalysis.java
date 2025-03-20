d57a24e6f16ebf575c1a2f5a4a98a70620a9a66f
aidant11 <aidant10@nycstudents.net>
2025-03-20 15:25:01 +0000

    123

--- ReviewAnalysis.java
+++ ReviewAnalysis.java
@@ -22,7 +22,12 @@ public class ReviewAnalysis {
     {
         for (int i=0; i<allReviews.length; i++)
         {
-            String s=i + "-" + allReviews[i].getComment():
+            String s=i + "-" + allReviews[i].getComment();
+            String last = s.substring(s.length()-1);
+            if (!last.equals(".") && !last.equals("!")) s+=".";
+            if (s.indexOf("!")>0) comments.add(s);
         }
+        return comments;
+        
     }
 }
